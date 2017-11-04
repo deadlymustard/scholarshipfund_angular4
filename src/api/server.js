b0f9fdf5752c4edb29a9816a8605117473858214
Shaner <regan.shaner@mlb.com>
2017-11-04 13:05:07 -0400

    another fix

--- src/api/server.js
+++ src/api/server.js
@@ -45,7 +45,7 @@ app.use(session({
 logger.debug("Setting up routes");
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  res.sendFile('../../dist/index.html');
 });
 // Setting up routes
 app.get('/', routes.index);
